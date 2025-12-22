3370f100d509e6feb2ab3f7a916e40aa1f2d1a81
JungHo-K1m <qopqop55@naver.com>
2025-12-22 15:36:14 +0900

    Update SDK version

--- granite.config.ts
+++ granite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     displayName: '헬스히어로', // 화면에 노출될 앱의 한글 이름으로 바꿔주세요.
     primaryColor: '#3182F6', // 화면에 노출될 앱의 기본 색상으로 바꿔주세요.
     icon: "https://static.toss.im/appsintoss/229/70577e23-0f8a-4d19-bce2-2e431a04bb26.png",
-    bridgeColorMode: 'inverted',
+    // bridgeColorMode: 'inverted',
   },
   web: {
     host: 'localhost',
